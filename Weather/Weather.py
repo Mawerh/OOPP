6bda6715aa5df5c6c1e0cf2df13ad869cf433aa6
J-I-M-S <32917926+J-I-M-S@users.noreply.github.com>
2017-12-12 14:12:16 +0800

    Fixing Html and Weath.py

--- Weather/Weather.py
+++ Weather/Weather.py
@@ -3,11 +3,19 @@ import requests
 
 app = Flask(__name__)
 
+@app.route('/WeatherSettings')
 
-@app.route('/home')
+def settings():
+    city = "Singapore"
+    r = requests.get("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=53e032f7efd36727cf5fa955bb4ffeb1")
+    json_object = r.json()
+    Weather = str(json_object["weather"]["main"])
+    WeatherDescription = str(json_object["weather"]["description"])
+    return render_template('WeatherSettings.html', weath=Weather, descrip=WeatherDescription)
+
+@app.route("/WeatherHome")
 def home():
-    return render_template('WeatherHome.html')
-
+    return render_template("WeatherHome.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
