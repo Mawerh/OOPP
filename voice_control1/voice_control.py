4d678637a6928445171d9292404ba5333c327afd
Ho Joey <170852T@mymail.nyp.edu.sg>
2017-12-12 15:37:09 +0800

    import functions

--- voice_control1/voice_control.py
+++ voice_control1/voice_control.py
@@ -1,11 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from voice_functions import *
 
 app = Flask(__name__)
 
+#allow user to input commands as well
 
 @app.route('/voice')
-def hello_world():
+def page():
     return render_template('voice.html')
+
+@app.route('/voice/function')
+def call_function():
+    vision(data=recordAudio())
+    return playsound('C:\\Users\\Ho Joey\\Documents\\GitHub\\OOPP\\voice_control1\\sound.mp3')
+
+
+#greetings = recordAudio()
+#tts = gTTS(text=greetings, lang='en')
+#tts.save('sound.mp3')
+#os.system('sound.mp3')
+#while True:
+#vision(data=recordAudio())
+#playsound('C:\\Users\\Ho Joey\\Documents\\GitHub\\OOPP\\voice_control1\\sound.mp3')
+#os.remove('sound.mp3')
+#mp3 = MP3('sound.mp3')
+#mp3.delete()
+
+#def play():
+    #playsound('C:\\Users\\Ho Joey\\Documents\\GitHub\\OOPP\\voice_control1\\sound.mp3')
 
 
 if __name__ == '__main__':
