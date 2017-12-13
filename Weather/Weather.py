880798fb684ff57dea9c1ad0e2d9edecf4afc313
J-I-M-S <32917926+J-I-M-S@users.noreply.github.com>
2017-12-13 11:07:28 +0800

    Updates on Weather.py and Weather Settings

--- Weather/Weather.py
+++ Weather/Weather.py
@@ -3,19 +3,13 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/WeatherSettings')
-
-def settings():
-    city = "Singapore"
-    r = requests.get("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=53e032f7efd36727cf5fa955bb4ffeb1")
-    json_object = r.json()
-    Weather = str(json_object["weather"]["main"])
-    WeatherDescription = str(json_object["weather"]["description"])
-    return render_template('WeatherSettings.html', weath=Weather, descrip=WeatherDescription)
-
-@app.route("/WeatherHome")
-def home():
+@app.route("/")
+def WeatherHome():
     return render_template("WeatherHome.html")
 
+@app.route('/WeatherSettings')
+def WeatherSettings():
+    return render_template("WeatherSettings.html")
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
