c21254a2d304b0c8df7ec6c8895f270cce2562c7
J-I-M-S <32917926+J-I-M-S@users.noreply.github.com>
2017-12-13 19:17:22 +0800

    Added gif's to the html. Completed weather api and aligning the page

--- Weather/Weather.py
+++ Weather/Weather.py
@@ -3,16 +3,16 @@ import requests
 
 app = Flask(__name__)
 
-@app.route("/")
-def WeatherHome():
+@app.route("/WeatherHome")
+def weatherHome():
     return render_template("WeatherHome.html")
 
 @app.route("/WeatherStart")
-def WeatherStart():
+def weatherStart():
     return render_template("WeatherStart.html")
 
 @app.route('/WeatherSettings', methods=['POST'])
-def home():
+def settings():
     r = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Singapore,SG&appid=53e032f7efd36727cf5fa955bb4ffeb1")
     json_object = r.json()
     temp_k = float(json_object["main"]["temp"])
@@ -20,7 +20,7 @@ def home():
     wind_speed = float(json_object['wind']['speed'])
     weather = json_object['weather'][0]["main"]
     weather_descrip = json_object['weather'][0]["description"]
-    temp_c = temp_k - 273.15
+    temp_c = int(temp_k - 273.15)
     return render_template('WeatherSettings.html', temp=temp_c, humid=humidity, windspd=wind_speed, weath=weather, descrip=weather_descrip)
 
 
