7eee9f45d30f906d16f5341312461183552775ec
J-I-M-S <32917926+J-I-M-S@users.noreply.github.com>
2018-02-04 05:40:22 +0800

    Updated Html for Homepage

--- Homepage/Homepage.py
+++ Homepage/Homepage.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request
-import requests
 
 app = Flask(__name__)
 
-
 @app.route("/Homepage")
-def homepageStart():
-   return render_template("Homepage.html")
+def HomepageStart():
+
+    return render_template("Homepage.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
